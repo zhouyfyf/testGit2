aa71304bd50c28a80bc4a8d9adbc14c72511b88f
xiaozhou <15827170978@163.com>
2020-07-26 21:48:32 +0800

    test

---
+++ src/main/java/com/zyf/pojo/User.java
@@ -0,0 +1,11 @@
+package com.zyf.pojo;
+
+/**
+ * @author: 周云飞
+ * @date:2020/7/26
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/com/zyf/pojo/UserTest.java
@@ -0,0 +1,9 @@
+package com.zyf.pojo;
+
+/**
+ * @author: 周云飞
+ * @date:2020/7/26
+ */
+public class UserTest {
+    //zyf
+}
